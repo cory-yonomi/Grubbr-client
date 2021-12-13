20f62a2f96a182b8c565e77032604b7c964b799e
Eddycodes27 <Eddycodes27@gmail.com>
2021-12-13 13:36:35 -0500

    Added css and some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
+import './Home.css'
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			<div className="intro">
+				<h1>Grubbr</h1>
+				<h4>"The Tinder For Food"</h4>
+				<p>Meet people in your area based on your common interests in restaurants...or not?</p>
+			</div>
 		</>
 	)
 }
