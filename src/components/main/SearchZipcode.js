0abe2be7167486c42fd8e27cac0f42d501ad21c8
LindZCoding <Lindzgaming@gmail.com>
2021-12-15 10:57:54 -0800

    revised zipcode form and navigation after clicking submit

--- src/components/main/SearchZipcode.js
+++ src/components/main/SearchZipcode.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from "axios"
 
 
@@ -8,26 +8,37 @@ const SearchZipcode = (props) => {
     const [input, setInput] = useState('')
     const [subValue, setSubValue] = useState('')
 
+    const navigate = useNavigate()
+
     // gets value of each number typed in the input zipcode field
     const handleChange = (e) => {
         // console.log('input value', e.target.value)
         setInput(e.target.value)
     }
 
+
     // gets whole value of zipcode entered once submit is clicked and searches for restaurants specfic to that zipcode
-    const submit = () => {
+    const submit = (e) => {
+        e.preventDefault()
         // console.log('submit value', input)
         setSubValue(input)
-        axios.get(`http://localhost:8000/restaurants/Yelp/${input}`, {
-            headers: {
-                "Authorization": `Bearer ${props.user.token}`
-            }
-        })
-            .then(foundRestaurants => {
-                // console.log('rests', foundRestaurants)
-                props.setRestaurants(foundRestaurants.data.businesses)
+        if (input.length === 5) {
+            axios.get(`http://localhost:8000/restaurants/Yelp/${input}`, {
+                headers: {
+                    "Authorization": `Bearer ${props.user.token}`
+                }
             })
-            .catch(err => console.log(err))
+                // maybe use filter function
+                .then(foundRestaurants => {
+                    // console.log('rests', foundRestaurants)
+                    props.setRestaurants(foundRestaurants.data.businesses)
+                })
+                .then(() => navigate('/restaurant-slide'))
+                .catch(err => console.log(err))
+                
+        } else {
+            return alert('Please enter a valid zipcode')
+        }
 
     }
 
@@ -38,8 +49,9 @@ const SearchZipcode = (props) => {
                     <form>
                         <label htmlFor="zipcode">Search by Zipcode:</label>
                         <input type="number" onChange={handleChange} />
+                        <button className='submitbutton' onClick={submit}>Submit</button>
                     </form>
-                    <Link to='/restaurant-slide'><button className='submitbutton' onClick={submit}>Submit</button></Link>
+                    {/* <Link to='/restaurant-slide'></Link> */}
                 </div>
             </div>
         </div>
