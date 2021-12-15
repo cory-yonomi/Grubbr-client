23c3523f91afed57fcd9e36cd1de6e3bff1d510b
Eddycodes27 <Eddycodes27@gmail.com>
2021-12-15 16:55:27 -0500

    added styling to restaurant-profile

--- src/components/main/RestaurantProfile.js
+++ src/components/main/RestaurantProfile.js
@@ -9,7 +9,24 @@ const RestaurantProfile = (props) => {
                <img src={props.likedRestaurant.image_url} alt={props.likedRestaurant.name}/>
                <h4>{mapCategories}</h4>
                <div className='othersLiked'>
-                <h2 className='othersWhoLiked'>Others Who Liked This Restaurant:</h2>
+                <h4 className='othersWhoLiked'>Others Who Liked This Restaurant:</h4>
+                <div className='nameMatch'>
+                {/* <img src={}/> */}
+                <label>Name</label>
+                <button>Add as Match</button>
+                </div>
+                <div className='nameMatch2'>
+                {/* <img src={}/> */}
+                <label>Name 2</label>
+                <button>Add as Match</button>
+                </div>
+                </div>
+                <div className='othersLiked'>
+                <h4>Restaurant Reviews From Other Users:</h4>
+                <div className='name'>
+                <h6>Name</h6>
+                <h6>Name 2</h6>
+                </div>
                 </div>
             </div>
         )
