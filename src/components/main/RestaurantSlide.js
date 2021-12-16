0402885223f90fd1f76321cf87ea12bb56e2da17
anaismveras <verasmanais@gmail.com>
2021-12-16 09:44:54 -0500

    revised heart button in restaurant slide

--- src/components/main/RestaurantSlide.js
+++ src/components/main/RestaurantSlide.js
@@ -2,7 +2,12 @@ import { useEffect, useState } from "react"
 import { Link } from 'react-router-dom'
 import axios from "axios"
 import '../css/RestaurantSlide.css'
+import Heart from '../images/HeartRestaurant.png'
 
+const ButtonStyle = {
+    width: '100px',
+    height: '100px'
+}
 
 const RestaurantSlide = (props) => {
 
@@ -15,7 +20,7 @@ const RestaurantSlide = (props) => {
                 </div>
                 <div className='heart-button'>
                     <button className='swipeButton' onClick={props.nextButton}>X</button>
-                    <Link to='/restaurant-profile'><button className='swipeButton' onClick={props.heartButton}>❤️</button></Link>
+                    <Link to='/restaurant-profile'><button className='swipeButton' onClick={props.heartButton}><img style={ButtonStyle} src={Heart} alt="Like Restaurant Button" /> </button></Link>
                 </div>
             </div >
         )
