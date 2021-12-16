1b47baa0f0e3f2d13248761c0f4fdb7287f2b6c8
Eddycodes27 <Eddycodes27@gmail.com>
2021-12-16 13:56:57 -0500

    Fixed resaturant-profile div elements

--- src/components/main/RestaurantProfile.js
+++ src/components/main/RestaurantProfile.js
@@ -7,21 +7,22 @@ const RestaurantProfile = (props) => {
 
 
         <div className='restaurantsProfile'>
-            <div className='container'>
+            <div className='profileContainer'>
                 <h1>{props.likedRestaurant.name}</h1>
                 <img src={props.likedRestaurant.image_url} alt={props.likedRestaurant.name} />
                 <h4>{mapCategories}</h4>
-
                 <h6>Cuisine: {props.likedRestaurant.categories}</h6>
                 <h6>Address: {props.likedRestaurant.location}</h6>
-                <h6>Rating: {props.likedRestaurant.rating} Stars</h6>
+                <h6>Rating: {props.likedRestaurant.rating}</h6>
                 <h6>Price Rating: {props.likedRestaurant.price}</h6>
             </div>
+            <div className='bottomDiv'>
             <div className='othersLiked'>
-                <h3 className='othersWhoLiked'>Others Who Liked This Restaurant:</h3>
+                <h3>Others Who Liked This Restaurant:</h3>
             </div>
-            <div className='othersLiked'>
+            <div className='othersReview'>
                 <h3>Restaurant Reviews From Other Users:</h3>
+            </div>
             </div>
         </div>
     )
