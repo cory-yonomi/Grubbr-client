3908f560b3e0eaa99195ff1ecd7f9cf7baf8f8dd
anaismveras <verasmanais@gmail.com>
2021-12-16 10:12:19 -0500

    revised buttons on restaurant slider page

--- src/components/main/RestaurantSlide.js
+++ src/components/main/RestaurantSlide.js
@@ -1,31 +1,39 @@
-import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
-import axios from "axios"
-import '../css/RestaurantSlide.css'
-import Heart from '../images/HeartRestaurant.png'
-import brokenHeart from '../images/disheartRestaurant.png'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "../css/RestaurantSlide.css";
+import Heart from "../images/HeartRestaurant.png";
+import brokenHeart from "../images/disheartRestaurant.png";
 
 const ButtonStyle = {
-    width: '100px',
-    height: '100px'
-}
+  width: "100px",
+  height: "100px",
+  margin: '10px'
+};
 
 const RestaurantSlide = (props) => {
+  return (
+    <div className="slide-page">
+      <div className="map-restaurants">
+        <div>{props.mapRestaurants}</div>
+      </div>
+          <div className='dislikeButton'>
+        <button className="swipeButton" onClick={props.nextButton}>
+          <img
+            src={brokenHeart}
+            style={ButtonStyle}
+            alt="Dislike Restaurant Button"
+          />
+        </button>
+        <Link to="/restaurant-profile">
+          <button className="swipeButton" onClick={props.heartButton}>
+            <img style={ButtonStyle} src={Heart} alt="Like Restaurant Button" />
+          </button>
+        </Link>
+    
+      </div>
+    </div>
+  );
+};
 
-        return (
-            <div className='slide-page'>
-                <div className='map-restaurants'>
-                <div >
-                    {props.mapRestaurants}
-                </div>
-                </div>
-                <div className='heart-button'>
-                    <button className='swipeButton' onClick={props.nextButton}><img src={brokenHeart} style={ButtonStyle} alt="Dislike Restaurant Button" /></button>
-                    <Link to='/restaurant-profile'><button className='swipeButton' onClick={props.heartButton}><img style={ButtonStyle} src={Heart} alt="Like Restaurant Button" /></button></Link>
-                </div>
-            </div >
-        )
-
-    }
-
-export default RestaurantSlide
+export default RestaurantSlide;
