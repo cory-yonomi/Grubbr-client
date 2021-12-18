f85dd01d6e02eb774828f068575534d99fdfb973
ornery-mouse <cory.sorel@gmail.com>
2021-12-17 20:08:21 -0600

    working on matches functionality

--- src/components/main/RestaurantProfile.js
+++ src/components/main/RestaurantProfile.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import apiUrl from '../../apiConfig'
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
+// import apiUrl from '../../apiConfig'
 import '../css/RestaurantProfile.css'
 import CreateComment from './CreateComment'
 
@@ -10,28 +10,10 @@ const RestaurantProfile = (props) => {
         return <p>{comment.body}</p>
     })
 
-    const restaurantCategories = props.likedRestaurant.categories
     const mapCategories = console.log(props.likedRestaurant)
 
-    const mapUsers = props.likedRestaurant.users
     console.log('array of users', props.likedRestaurant.users)
     console.log('likedRestaurant', props.likedRestaurant)
-    useEffect(() => {
-  
-
-        axios.get(`${apiUrl}/profile/restaurantLikers`, {
-            headers: {
-                "Authorization": `Bearer ${props.user.token}`
-            }
-        },
-            {
-                body: {
-                    userIds: props.restaurantLikers
-                }
-            }
-       )
-       .then(resp => console.log('response!', resp))
-    }, [])
 
     return (
 
@@ -68,38 +50,3 @@ const RestaurantProfile = (props) => {
 
 
 export default RestaurantProfile
-
-// import CreateComment from "./CreateComment"
-
-// const RestaurantProfile = (props) => {
-//     const restaurantCategories = props.likedRestaurant.categories
-//     const mapCategories = console.log(props.likedRestaurant)
-//     return (
-
-//         <div className='restaurantsProfile'>
-//             <div className='profileContainer'>
-//                 <h1>{props.likedRestaurant.name}</h1>
-//                 <img src={props.likedRestaurant.image_url} alt={props.likedRestaurant.name} />
-//                 <h4>{mapCategories}</h4>
-//                 <h6>Cuisine: {props.likedRestaurant.categories}</h6>
-
-//                 {/* <h6>Address: {props.likedRestaurant.location[0]}, {props.likedRestaurant.location[1]}</h6> */}
-
-//                 <h6>Rating: {props.likedRestaurant.rating}</h6>
-//                 <h6>Price Rating: {props.likedRestaurant.price}</h6>
-//             </div>
-//             <div className='bottomDiv'>
-//             <div className='othersLiked'>
-//                 <h3>Others Who Liked This Restaurant:</h3>
-//             </div>
-//             <div className='othersReview'>
-//                 <h3>Restaurant Reviews From Other Users:</h3>
-//                 <CreateComment comment={props.postComment} setComment={props.setComment} user={props.user}/>
-//             </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-
-// export default RestaurantProfile
