710c2ef941ec91464247fd33549a1c8c0f7e44d5
anaismveras <verasmanais@gmail.com>
2021-12-18 20:58:25 -0500

    added styling to resturant profile

--- src/components/main/RestaurantProfile.js
+++ src/components/main/RestaurantProfile.js
@@ -3,91 +3,101 @@ import { captureRejectionSymbol } from 'events'
 import { useEffect, useState } from 'react'
 import '../css/RestaurantProfile.css'
 import CreateComment from './CreateComment'
-import apiUrl from "../../apiConfig"
-
+import apiUrl from '../../apiConfig'
+import drakeImg from '../images/DrakeExampleImg.jpeg'
 
 const RestaurantProfile = (props) => {
+	const deleteYourComment = (e) => {
+		e.preventDefault()
 
-    const deleteYourComment = (e) => {
-        e.preventDefault()
+		axios
+			.delete(
+				`${apiUrl}/comments/${props.likedRestaurant._id}/${e.target.id}`,
+				{
+					headers: {
+						Authorization: `Bearer ${props.user.token}`,
+					},
+				}
+			)
+			.then((comment) => {
+				console.log('COMMENT DELETED', comment)
+			})
+			// .then(() => navigate('/profile'))
+			.catch((err) => console.log(err))
+	}
 
-        axios.delete(`${apiUrl}/comments/${props.likedRestaurant._id}/${e.target.id}`, {
-           
-                headers: {
-                    "Authorization": `Bearer ${props.user.token}`
-                },
-            }
-        )
-            .then(comment => {
-                console.log('COMMENT DELETED', comment)
-            })
-            // .then(() => navigate('/profile'))
-            .catch(err => console.log(err))
-    }
+	const commentArray = props.likedRestaurant.comments.map((comment) => {
+		// console.log('comment body', comment)
+		return (
+			<div>
+				<p>{comment.body}</p>
+				<p>
+					<button id={comment._id} onClick={deleteYourComment}>
+						Delete
+					</button>
+				</p>
+			</div>
+		)
+	})
 
+	// const commentIdArray = props.likedRestaurant.comments.map(comment => {
+	//     console.log('comment body id array', comment._id)
+	//     return (<p>{comment._id}</p>)
+	// })
 
-    const commentArray = props.likedRestaurant.comments.map(comment => {
-        // console.log('comment body', comment)
-        return (
-            <div>
-            <p>{comment.body}</p>
-            <p><button id={comment._id} onClick={deleteYourComment}>Delete</button></p>
-            </div>
-        ) 
-    })
+	const restaurantCategories = props.likedRestaurant.categories
+	// const mapCategories = console.log(props.likedRestaurant)
 
-    // const commentIdArray = props.likedRestaurant.comments.map(comment => {
-    //     console.log('comment body id array', comment._id)
-    //     return (<p>{comment._id}</p>)
-    // })
+	const mapUsers = props.likedRestaurant.users
+	console.log('array of users', props.likedRestaurant.users)
+	console.log('likedRestaurant', props.likedRestaurant)
 
+	const likersArray = props.restaurantLikers.map((liker) => {
+		console.log('this is the array of likers', liker)
+		return (
+			<div className='likerDiv'>
+                <img src={drakeImg} alt="" srcset="" />
+				<p>{liker.firstName} </p>
+                <button>Add as Match</button>
+			</div>
+		)
+	})
 
-    const restaurantCategories = props.likedRestaurant.categories
-    const mapCategories = console.log(props.likedRestaurant)
-
-    const mapUsers = props.likedRestaurant.users
-    console.log('array of users', props.likedRestaurant.users)
-    console.log('likedRestaurant', props.likedRestaurant)
-
-    const likersArray = props.restaurantLikers.map(liker => {
-        return <p>{liker.firstName}</p>
-    })
-
-    return (
-
-        <div className='restaurantsProfile'>
-            <div className='profileContainer'>
-                <h1>{props.likedRestaurant.name}</h1>
-                <img src={props.likedRestaurant.image_url} alt={props.likedRestaurant.name} />
-                <h4>{mapCategories}</h4>
-                <h6>Cuisine: {props.likedRestaurant.categories}</h6>
-
-
-                {/* <h6>Address: {props.likedRestaurant.location[0]}, {props.likedRestaurant.location[1]}</h6> */}
-
-
-                <h6>Rating: {props.likedRestaurant.rating}</h6>
-                <h6>Price Rating: {props.likedRestaurant.price}</h6>
-            </div>
-            <div className='bottomDiv'>
-                <div className='othersLiked'>
-                    <h3>Others Who Liked This Restaurant:</h3>
-                    {likersArray}
-                </div>
-                <div className='othersReview'>
-                    <h3>Restaurant Reviews From Other Users:</h3>
-                    {commentArray}
-                    <CreateComment comment={props.postComment} user={props.user} setComment={props.setComment} />
-                    <div>
-                        <div>
-                            {/* <DeleteComment likedRestaurant={props.likedRestaurant} user={props.user} commentIdArray={commentIdArray} setComment={props.setComment} /> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="restaurantsProfile">
+			<div className="profileContainer">
+				<h1>{props.likedRestaurant.name}</h1>
+				<img
+					src={props.likedRestaurant.image_url}
+					alt={props.likedRestaurant.name}
+				/>
+				<h6>Cuisine: {props.likedRestaurant.categories}</h6>
+				<h6>Address: {props.likedRestaurant.location}</h6>
+				<h6>Rating: {props.likedRestaurant.rating}</h6>
+				<h6>Price Rating: {props.likedRestaurant.price}</h6>
+			</div>
+			<div className="bottomDiv">
+				<div className="othersLiked">
+					<h3>Others Who Liked This Restaurant:</h3>
+					{likersArray}
+				</div>
+				<div className="othersReview">
+					<h3>Restaurant Reviews From Other Users:</h3>
+					{commentArray}
+					<CreateComment
+						comment={props.postComment}
+						user={props.user}
+						setComment={props.setComment}
+					/>
+					<div>
+						<div>
+							{/* <DeleteComment likedRestaurant={props.likedRestaurant} user={props.user} commentIdArray={commentIdArray} setComment={props.setComment} /> */}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
-
 
 export default RestaurantProfile
